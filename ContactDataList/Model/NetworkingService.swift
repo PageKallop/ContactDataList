c67b760571eb6cb74b02a38633c57baf1ad6b351
PageKallop <PageMarie78@gmail.com>
2021-03-23 12:51:20 -0500

    Create NSManaged object

--- ContactDataList/Model/NetworkingService.swift
+++ ContactDataList/Model/NetworkingService.swift
@@ -12,42 +12,86 @@ class NetworkingService {
     
     let persistence = PersistentService.shared
     
-    var contactList = [ContactData]()
+    var contactListFull = [ContactData]()
     
     init() {
         getContacts()
     }
-    
+
     func getContacts() {
-        
+
         if let path = Bundle.main.url(forResource: "myJsonFile0", withExtension: "json") {
             print(path)
-            
+         
             do {
+
                 let data = try Data(contentsOf: path)
                 let jsonDecoder = JSONDecoder()
-            
-       
+
                 let dataFromJson = try jsonDecoder.decode(ContactData.self, from: data)
                 
+                if let json = try  JSONSerialization.jsonObject(with: data, options: [.mutableContainers]) as? [String: AnyObject] {
+                    print(json)
+                    guard let itemsJsonArray = json["contact"] as? [[String: AnyObject]]
+                   
+                    else { return
+                    }
+            
+                }
+                
+              func createContactEntity(dictionary: [String: AnyObject]) {
+                
                 let contactList = dataFromJson.contacts
-               
                 let listOfContacts = Contact(context: persistence.context)
+                    
+                    let context = PersistentService.shared.persistentContainer.viewContext
+                    if let contactEntity = NSEntityDescription.insertNewObject(forEntityName: "Contact", into: context) as? Contact {
+                        contactEntity.id = dictionary["id"] as? UUID
+                        contactEntity.firstName = dictionary["firstName"] as? String
+                        contactEntity.lastName = dictionary["lastName"] as? String
+                        contactEntity.streetAddress = dictionary["streetAddress"] as? String
+                        contactEntity.city = dictionary["city"] as? String
+                        contactEntity.state = dictionary["state"] as? String
+                        contactEntity.postalCode = dictionary["postalCode"] as? String
+                        contactEntity.type = dictionary["type"] as? String
+                        contactEntity.number = dictionary["number"] as? String
+                        
+                      
+//                        self.persistence.save()
+                        
+                    }
+                    
+                   print(context)
+                    
                 
-                listOfContacts.id = contactList[0].id
-                listOfContacts.firstName = contactList[0].firstName
-                listOfContacts.lastName = contactList[0].lastName
-                listOfContacts.streetAddress = contactList[0].address.streetAddress
-                listOfContacts.city = contactList[0].address.city
-                listOfContacts.state = contactList[0].address.state
-                listOfContacts.postalCode = contactList[0].address.postalCode
-                listOfContacts.type = contactList[0].phoneNumber.type
-                listOfContacts.number = contactList[0].phoneNumber.number
+                   
+                }
                 
-                    self.persistence.save()
+  
+            
+
+//                let contactList = dataFromJson.contacts
+//
+//                let listOfContacts = Contact(context: persistence.context)
                 
- 
-                print(listOfContacts)
+                
+              
+//
+//                listOfContacts.firstName = contactList[0].firstName
+//                listOfContacts.lastName = contactList[0].lastName
+//                listOfContacts.streetAddress = contactList[0].address.streetAddress
+//                listOfContacts.city = contactList[0].address.city
+//                listOfContacts.state = contactList[0].address.state
+//                listOfContacts.postalCode = contactList[0].address.postalCode
+//                listOfContacts.type = contactList[0].phoneNumber.type
+//                listOfContacts.number = contactList[0].phoneNumber.number
+//
+//
+//
+//                    self.persistence.save()
+
+
+            
                 
                 
             }catch {
@@ -55,8 +99,11 @@ class NetworkingService {
             }
         }
         
-        
+   
+
+
     }
+
 
     
 }
--- ContactDataList/ViewController.swift
+++ ContactDataList/ViewController.swift
@@ -24,10 +24,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         loadSavedData()
         self.tableView.delegate = self
         self.tableView.dataSource = self
-        // Do any additional setup after loading the view.
-        
+      
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
+    
+
 
     let tableView : UITableView = {
         let table = UITableView()
@@ -85,41 +86,41 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         let list = fetchedResultsController.object(at: indexPath)
         
-        cell.textLabel?.text = list.firstName
+        cell.textLabel?.text = list.city
         return cell 
     }
     
     
     
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-        switch type {
-        case .insert:
-            tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
-        case .delete:
-            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
-        case .move:
-            break
-        case .update:
-            break
-        @unknown default:
-            print("Error")
-        }
-    }
+//    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+//        switch type {
+//        case .insert:
+//            tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
+//        case .delete:
+//            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
+//        case .move:
+//            break
+//        case .update:
+//            break
+//        @unknown default:
+//            print("Error")
+//        }
+//    }
     
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        switch type {
-        case .insert:
-            tableView.insertRows(at: [newIndexPath!], with: .fade)
-        case .delete:
-            tableView.deleteRows(at: [indexPath!], with: .fade)
-        case .update:
-            tableView.reloadRows(at: [indexPath!], with: .fade)
-        case .move:
-            tableView.moveRow(at: indexPath!, to: newIndexPath!)
-        @unknown default:
-            print("Error")
-        }
-    }
+//    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+//        switch type {
+//        case .insert:
+//            tableView.insertRows(at: [newIndexPath!], with: .fade)
+//        case .delete:
+//            tableView.deleteRows(at: [indexPath!], with: .fade)
+//        case .update:
+//            tableView.reloadRows(at: [indexPath!], with: .fade)
+//        case .move:
+//            tableView.moveRow(at: indexPath!, to: newIndexPath!)
+//        @unknown default:
+//            print("Error")
+//        }
+//    }
 
 }
 
